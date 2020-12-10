02474cfda1c24556f4ae23b18de581da79de52c7
Leon RO <leon.rodriguez.ore@gmail.com>
2020-12-10 13:08:58 -0300

    creación de metodo suma de números por pantalla

--- app/src/main/java/com/example/alfa/MainActivity.java
+++ app/src/main/java/com/example/alfa/MainActivity.java
@@ -10,7 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
-}
+    private int add (String values) {
+        if (values.isEmpty()) {
+            return 0;
+        } else if (values.length() == 3){
+            String valuesArray[] = values.split(",");
+            int result = Integer.parseInt(valuesArray[0]) + Integer.parseInt(valuesArray[1]);
+            return result;
+        } else {
+            return Integer.parseInt(values);
+        }
+        }
+    }
