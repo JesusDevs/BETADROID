88c754a9f9d241b5b55154fe54163ee0b5735ec5
Leon RO <leon.rodriguez.ore@gmail.com>
2020-12-13 21:58:41 -0300

    creación de metodo suma de números por pantalla, el usuario ingresa datos por pantalla tactil

--- app/src/main/java/com/example/alfa/Calculadora.java
+++
@@ -1,4 +0,0 @@
-package com.example.alfa;
-
-public class Calculadora {
-}
--- app/src/main/java/com/example/alfa/MainActivity.java
+++ app/src/main/java/com/example/alfa/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,18 +16,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int resultado1= add("");
+        /*int resultado1= add("");
         Log.d("resultado1",String.valueOf(resultado1));
-
         int resultado2= add("3");
         Log.d("resultado2",String.valueOf(resultado2));
-
         int resultado3= add("3,4");
-        Log.d("resultado3",String.valueOf(resultado3));
+        Log.d("resultado3",String.valueOf(resultado3))*/
 
-        TextView textView =findViewById(R.id.textView2);
-        textView.setText("hola mundo ");
+        //Actividad número 2
+
+        final TextView textView =findViewById(R.id.textView2);
+        final EditText editText1 = findViewById(R.id.editTextNumber);
+        final EditText editText2 = findViewById(R.id.editTextNumber2);
+        Button botonCalcular =findViewById(R.id.button);
+
+
+
+    botonCalcular.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        int numero1=0;
+        int numero2=0;
+        numero1 =Integer.parseInt(editText1.getText().toString());
+        numero2 =Integer.parseInt(editText2.getText().toString());
+        int resultado = (numero1+numero2);
+        textView.setText(" Resultado: "+resultado);
     }
+    });
+
+    }
+
+
     private int add (String values) {
         if (values.isEmpty()) {
             return 0;
